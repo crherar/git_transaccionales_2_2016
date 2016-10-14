6ce1923e36869d5dcb8194f51dea834cb91923c0
Matias Alvarez Sabate <matialvarezs@gmail.com>
2016-10-14 19:50:44 -0300

    archivo compilatodo.sh con los formularios - archivos inp y c, antes hay que darle permisos chmod 777 compilatodo.sh y despues ./compilatodo.sh

--- DEMONIO/compilatodo.sh
+++ DEMONIO/compilatodo.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+
+
+
+echo "*************** usuarios *************** ";
+gmon regusr.inp regusr.c
+
+echo "****************************************** ";
+
+echo "*************** login ***************";
+gmon loginn.inp loginn.c
+
+echo "****************************************** ";
+
+echo "*************** prestamos ***************";
+gmon registrar_prestamo.inp hprest.c
+
+echo "****************************************** ";
+
+echo "*************** objetos ***************";
+gmon registrar_objeto.inp regobj.c
+
+echo "****************************************** ";
+
+echo "*************** menus ***************\n";
+gmon menu_principal.inp menu objetos.inp menpri.c menobj.c
+
+echo "****************************************** ";
+
 rm DEMONIO
 ecpg dbm_conectar.pgc dbm_prestamos.pgc dbm_usuarios.pgc dbm_objetos.pgc  
 gcc -I /usr/include/postgresql dbm_conectar.c dbm_usuarios.c dbm_prestamos.c dbm_objetos.c DEMONIO.c -o DEMONIO -L /usr/lib/postgresql/9.0/lib -lecpg
