5540126dd67603bc01cdbdc1b91a3d64ef2d6361
Matias Alvarez Sabate <matialvarezs@gmail.com>
2016-11-05 20:39:18 -0300

    ejemplo llamada de demonio

--- SEGUNDA PARTE/WWW/PY/servidor.py
+++ SEGUNDA PARTE/WWW/PY/servidor.py
@@ -1,5 +1,6 @@
 import socket  
 import json 
+import usuarios
 # Create a TCP/IP socket  
 socket_php = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
  
@@ -10,7 +11,7 @@ socket_php.bind(server_address)
  
 # Listen for incoming connections  
 socket_php.listen(10)  
- 
+usuarios usr = usuarios.usuarios()
 while True:  
  # Wait for a connection  
  print 'Esperando para conectarse'  
@@ -25,7 +26,9 @@ while True:
        
  data = ''  
        
- data = connection.recv(2000,socket.MSG_DONTWAIT)        
+ data = connection.recv(2000,socket.MSG_DONTWAIT)
+ if data == "reguser"
+ 	usr.insertar_usuario(data)
  print 'data recibida "%s"' % data
  print 'la direccion del cliente es: '+json.dumps(client_address)#str(connection)#str(client_address)
 # socket_php.send(str(int(datos_cliente[1])))
